ba8b6de76b3a765b177d11787ecfea6eac1dae4e
Rendy Artha <rendyartha312@gmail.com>
2025-07-23 11:24:43 +0700

    feat: setup foldering and layout styling

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OnboardingPages = () => {
+  return <div></div>;
+};
+
+export default OnboardingPages;
---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuizPages = () => {
+  return <div></div>;
+};
+
+export default QuizPages;
